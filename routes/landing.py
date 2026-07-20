09189ed501ef09d8fd36dce4bd13edc1448327f1
unknown <leighankhan56@gmail.com>
2026-07-20 09:51:13 +0300

    Updated all pages to WakalaPro theme (Green/Pink)

--- routes/landing.py
+++ routes/landing.py
@@ -1,8 +1,15 @@
 from flask import Blueprint, render_template
+from models.merchant import Merchant
+from models.operator import Operator
 
 landing = Blueprint('landing', __name__)
 
 @landing.route('/')
 def index():
     """Home page - Landing page"""
-    return render_template('landing.html')
+    merchants_count = Merchant.query.count()
+    operators_count = Operator.query.count()
+    
+    return render_template('landing.html',
+                         merchants_count=merchants_count,
+                         operators_count=operators_count)
